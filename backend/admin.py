f38c6aa960a724c44d2243defd34e55fd2e0a724
igor <igor.stoleski@mail.de>
2024-05-30 18:41:08 +0200

    add django-import-export

--- backend/admin.py
+++ backend/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from .models import Video
 from import_export import resources
-from import_export.admin import ImportExportActionModelAdmin
+from import_export.admin import ImportExportModelAdmin
 
 
 class VideoResources(resources.ModelResource):
     class Meta:
         model = Video
+        """ fields = {'title', 'created_at', 'description'} """
 
 
 @admin.register(Video)
-class VideoAdmin(ImportExportActionModelAdmin):
-    """ list_display = ('title', 'created_at', 'description') """
+class VideoAdmin(ImportExportModelAdmin):
     pass
+
--- videoflix/settings.py
+++ videoflix/settings.py
@@ -18,13 +18,17 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '10.10.0.100'
+]
 
 CACHE_TTL = 60 * 15
 
-INTERNAL_IPS = [
+""" INTERNAL_IPS = [
     "127.0.0.1",
-]
+] """
 
 
 # Application definition
