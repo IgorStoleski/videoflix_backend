ff8186aa219f2b4b1198326d973e8bd3ac654e6e
igor <igor.stoleski@mail.de>
2024-05-30 17:25:37 +0200

    add convert to 720p

--- backend/tasks.py
+++ backend/tasks.py
@@ -9,9 +9,9 @@ def convert_480p(source):
     
     
     
-""" def convert_720p(source):    
-    new_file_name = source + _720p.mp4
-    cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file)    
-    run = subprocess.run(cmd, capture_output=True, shell=True) """
+def convert_720p(source):    
+    target = source.replace('.mp4', '_720p.mp4')
+    cmd = f'ffmpeg -i "{source}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{target}"'    
+    subprocess.run(cmd, capture_output=True, shell=True)
     
     
